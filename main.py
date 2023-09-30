d46428e98569ff6035c724633cb15728f2f08038
Arthur-Vic <arthurvicto99@gmail.com>
2023-09-30 18:15:22 -0300

    got weather api working and rain detection system

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import requests
 
-OPEN_WEATHER_MAP_ENDPOINT = 'https://api.openweathermap.org/data/2.5/onecall'
-API_KEY = pass ###API KEY INSIDE A STRING GOES HERE
+OPEN_WEATHER_MAP_ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast'
+API_KEY = '' ###API KEY GOES HERE (STR)
 
 weather_params = {
-    'lat': -19.865008,
-    'long': -43.984186,
+    'lat':-19.865010,
+    'lon':-43.983825,
     'appid': API_KEY,
-    'exclude': 'current,minutely,daily',
 }
 
 response = requests.get(OPEN_WEATHER_MAP_ENDPOINT, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-cunrrent_weather_id = weather_data['hourly'][0]['weather'][0]['id']
+current_days_forecast = weather_data['list'][:5]
+# print(current_days_forecast)
+
+current_days_ids = []
+
+for hour in current_days_forecast:
+    weather_id = hour['weather'][0]['id']
+    current_days_ids.append(weather_id)
+    # print(weather_id)
+
+# print(current_days_ids)
+
+rain_today = False
+for id in current_days_ids:
+    if id < 700:
+        rain_today = True
+
+if rain_today == True:
+    print('Gonna rain today bitch')
+
+
+
+
