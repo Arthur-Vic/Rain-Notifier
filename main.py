fba216c41607f863d52fccf47f35c2e4cdb681b8
Arthur-Vic <arthurvicto99@gmail.com>
2023-09-30 20:05:16 -0300

    ready-ish for deploymente

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
+import smtplib
+from keys import API_KEY, MY_EMAIL, MY_PASSWORD
 
 OPEN_WEATHER_MAP_ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast'
-API_KEY = '' ###API KEY GOES HERE (STR)
+
+#-----------------WEATHER API-----------------------------
 
 weather_params = {
     'lat':-19.865010,
@@ -12,26 +15,41 @@ weather_params = {
 response = requests.get(OPEN_WEATHER_MAP_ENDPOINT, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
+
+
 current_days_forecast = weather_data['list'][:5]
-# print(current_days_forecast)
+# print(current_days_forecast) #Debugging
 
 current_days_ids = []
 
 for hour in current_days_forecast:
     weather_id = hour['weather'][0]['id']
     current_days_ids.append(weather_id)
-    # print(weather_id)
+    # print(weather_id) #Debugging
 
-# print(current_days_ids)
+# print(current_days_ids) #Debugging
 
 rain_today = False
 for id in current_days_ids:
     if id < 700:
         rain_today = True
 
+message = "Subject:Rain Expected Today\n\nIt might rain today; you should consider carrying an umbrella."
+
+#------------------GMAIL API-------------------------------------
+def rain_warning():
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=message)
+    except:
+        print('Error sendind the email')
+    else:
+        print('email sent')
+
+
 if rain_today == True:
-    print('Gonna rain today bitch')
-
-
-
+    # print(message) #Debugging
+    rain_warning()
 
